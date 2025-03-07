58b2f42fad6ca6d5c53a037d5b051b5aa63d93b2
Zub-Hub8 <ahmedzubaie206@gmail.com>
2025-03-07 15:08:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const schoolRoutes = require("./routes/schoolRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", schoolRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
